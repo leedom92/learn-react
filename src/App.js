967e605eaf288502fedb8552186f9f0042f0b80a
Leedom <leedom@jinlanzuan.com>
2021-02-26 14:03:21 +0800

    use animation coding

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Leedom is learning React</code>.
+        <p className="coding">
+          Leedom is learning React.
         </p>
       </header>
     </div>
